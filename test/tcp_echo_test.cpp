b7e0d30e41a515ac4d37a79fe69e0813ceb3c850
Stephen Newell <sjnewell@gmail.com>
2018-09-18 22:29:50 -0600

    Add echo tests

---
+++ test/tcp_echo_test.cpp
@@ -0,0 +1,69 @@
+#include <checkerboard/fns.hpp>
+
+#include <gtest/gtest.h>
+
+namespace
+{
+    template <checkerboard::Domain DOMAIN>
+    checkerboard::ListeningSocket<DOMAIN, checkerboard::stream>
+    make_server(checkerboard::Address<DOMAIN> const & address)
+    {
+        checkerboard::Socket<DOMAIN, checkerboard::stream> s;
+        auto bound_socket = checkerboard::bind(std::move(s), address);
+        return checkerboard::listen(std::move(bound_socket), 1);
+    }
+
+    template <checkerboard::Domain DOMAIN>
+    checkerboard::ConnectedSocket<DOMAIN, checkerboard::stream>
+    connect_server(checkerboard::Address<DOMAIN> const & address)
+    {
+        checkerboard::Socket<DOMAIN, checkerboard::stream> s;
+        return checkerboard::connect(std::move(s), address);
+    }
+} // namespace
+
+TEST(Tcp, echo_inet)
+{
+    std::uint8_t ip[4] = {127, 0, 0, 1};
+    checkerboard::Address<checkerboard::inet> a{ip, 0};
+    auto listening_socket = make_server(a);
+    auto outbound_connection = connect_server(listening_socket.address());
+    auto inbound_connection = checkerboard::accept(listening_socket);
+
+    std::uint8_t const hello[] = "Hello";
+    auto sent_bytes = checkerboard::send(outbound_connection, hello);
+
+    std::uint8_t world[6];
+    checkerboard::recv(std::get<0>(inbound_connection), world);
+    checkerboard::send(std::get<0>(inbound_connection), world);
+
+    std::uint8_t buffer[6];
+    auto received_bytes = checkerboard::recv(outbound_connection, buffer);
+
+    ASSERT_EQ(sent_bytes, received_bytes);
+    ASSERT_TRUE(
+        std::equal(std::begin(hello), std::end(hello), std::begin(buffer)));
+}
+
+TEST(Tcp, echo_inet6)
+{
+    std::uint8_t ip[16] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1};
+    checkerboard::Address<checkerboard::inet6> a{ip, 0};
+    auto listening_socket = make_server(a);
+    auto outbound_connection = connect_server(listening_socket.address());
+    auto inbound_connection = checkerboard::accept(listening_socket);
+
+    std::uint8_t const hello[] = "Hello";
+    auto sent_bytes = checkerboard::send(outbound_connection, hello);
+
+    std::uint8_t world[6];
+    checkerboard::recv(std::get<0>(inbound_connection), world);
+    checkerboard::send(std::get<0>(inbound_connection), world);
+
+    std::uint8_t buffer[6];
+    auto received_bytes = checkerboard::recv(outbound_connection, buffer);
+
+    ASSERT_EQ(sent_bytes, received_bytes);
+    ASSERT_TRUE(
+        std::equal(std::begin(hello), std::end(hello), std::begin(buffer)));
+}
---
+++ test/udp_echo_test.cpp
@@ -0,0 +1,58 @@
+#include <checkerboard/fns.hpp>
+
+#include <gtest/gtest.h>
+
+namespace
+{
+    template <checkerboard::Domain DOMAIN>
+    checkerboard::BoundSocket<DOMAIN, checkerboard::datagram>
+    make_server(checkerboard::Address<DOMAIN> const & address)
+    {
+        checkerboard::Socket<DOMAIN, checkerboard::datagram> s;
+        return checkerboard::bind(std::move(s), address);
+    }
+} // namespace
+
+TEST(Udp, echo_inet)
+{
+    std::uint8_t ip[4] = {127, 0, 0, 1};
+    checkerboard::Address<checkerboard::inet> a{ip, 0};
+    auto client = make_server(a);
+    auto server = make_server(a);
+
+    std::int8_t const hello[] = "Hello";
+    auto sent_bytes = checkerboard::send_to(client, server.address(), hello);
+
+    std::int8_t world[6];
+    checkerboard::recv_from(server, world);
+    checkerboard::send_to(server, client.address(), world);
+
+    std::int8_t buffer[6];
+    auto received_data = checkerboard::recv_from(client, buffer);
+
+    ASSERT_EQ(sent_bytes, std::get<0>(received_data));
+    ASSERT_TRUE(
+        std::equal(std::begin(hello), std::end(hello), std::begin(buffer)));
+}
+
+TEST(Udp, echo_inet6)
+{
+    std::uint8_t ip[16] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1};
+    checkerboard::Address<checkerboard::inet6> a{ip, 0};
+    auto client = make_server(a);
+    auto server = make_server(a);
+
+    std::uint8_t const hello[] = "Hello";
+    auto sent_bytes = checkerboard::send_to(client, server.address(), hello);
+
+    std::uint8_t world[6];
+    checkerboard::recv_from(server, world);
+    checkerboard::send_to(server, client.address(), world);
+
+    std::uint8_t buffer[6];
+    auto received_data = checkerboard::recv_from(client, buffer);
+
+    ASSERT_EQ(sent_bytes, std::get<0>(received_data));
+    ASSERT_TRUE(
+        std::equal(std::begin(hello), std::end(hello), std::begin(buffer)));
+}
