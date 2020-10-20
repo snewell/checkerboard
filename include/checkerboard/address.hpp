c18524cc3a1e4666c51adbf498da1524ef7ea6ba
Stephen Newell <sjnewell@gmail.com>
2020-10-20 12:20:19 -0600

    Separate common and inet-specfiic address stuff
    This is prep work to support address types other than inet and inet6.

--- include/checkerboard/address.hpp
+++ include/checkerboard/address.hpp
@@ -13,46 +13,75 @@ namespace checkerboard
 {
     namespace inner
     {
+        template <Domain DOMAIN>
+        class BaseAddress
+        {
+        public:
+            using sockaddr_type = sockaddr_type_t<DOMAIN>;
+
+            static constexpr ::socklen_t sockaddr_size = sizeof(sockaddr_type);
+
+            BaseAddress(sockaddr_type sockaddr)
+            {
+                ::memcpy(&_sockaddr, &sockaddr, sockaddr_size);
+            }
+
+            ::sockaddr const * sockaddr() const noexcept
+            {
+                return reinterpret_cast<::sockaddr const *>(&_sockaddr);
+            }
+
+        protected:
+            sockaddr_type _sockaddr;
+        };
+
+        template <Domain DOMAIN>
+        struct is_inet_domain
+        {
+            static constexpr bool value = false;
+        };
+
+        template <Domain DOMAIN>
+#if __cplusplus >= 201703L
+        inline
+#endif
+            constexpr auto is_inet_domain_v = is_inet_domain<DOMAIN>::value;
+
+        template <>
+        struct is_inet_domain<inet>
+        {
+            static constexpr bool value = true;
+        };
+        template <>
+        struct is_inet_domain<inet6>
+        {
+            static constexpr bool value = true;
+        };
     } // namespace inner
 
-    template <Domain DOMAIN>
-    class Address
+    template <Domain DOMAIN,
+              std::enable_if_t<inner::is_inet_domain_v<DOMAIN>, int> = 0>
+    class Address : public inner::BaseAddress<DOMAIN>
     {
     public:
-        using sockaddr_type = sockaddr_type_t<DOMAIN>;
-
-        static constexpr ::socklen_t sockaddr_size = sizeof(sockaddr_type);
-
         static constexpr std::size_t ip_byte_count = ip_size<DOMAIN>::value;
-
-        Address(sockaddr_type sockaddr)
-        {
-            ::memcpy(&_sockaddr, &sockaddr, sockaddr_size);
-        }
-
+        using inner::BaseAddress<DOMAIN>::BaseAddress;
         Address(std::uint8_t const (&ip)[ip_byte_count], std::uint16_t port)
           : Address{inner::sockaddr_builder<DOMAIN>::make_sockaddr(ip, port)}
         {
         }
 
-        ::sockaddr const * sockaddr() const noexcept
-        {
-            return reinterpret_cast<::sockaddr const *>(&_sockaddr);
-        }
-
         void ip(std::uint8_t (&ip)[ip_size_v<DOMAIN>])
         {
-            inner::sockaddr_ip_getter<DOMAIN>::fill_ip(_sockaddr, ip);
+            inner::sockaddr_ip_getter<DOMAIN>::fill_ip(
+                inner::BaseAddress<DOMAIN>::_sockaddr, ip);
         }
 
         std::uint16_t port() const noexcept
         {
             return inner::sockaddr_port_getter<DOMAIN>::get_sockaddr_port(
-                _sockaddr);
+                inner::BaseAddress<DOMAIN>::_sockaddr);
         }
-
-    private:
-        sockaddr_type _sockaddr;
     };
 } // namespace checkerboard
 
